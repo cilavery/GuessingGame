b9642d56912d85ce3ec5992db9c8983895bdf852
Cecilia Avery <cilsong@gmail.com>
2018-02-08 21:50:18 -0500

    Guessing Game!

---
+++ js/GuessingGame.js
@@ -0,0 +1,165 @@
+function generateWinningNumber() {
+   return (Math.floor(Math.random() * 100)+ 1);
+}
+
+Game.prototype.generateRandomNumber = function() {
+    var ranNum = 100 - this.playersGuess;
+    console.log(this.playersGuess, ranNum, this.isLower());
+
+    if (this.isLower()) {
+        return this.playersGuess + Math.floor(Math.random() * ranNum)
+    } else {
+        return Math.floor(Math.random() * ranNum)
+    }
+}
+
+function shuffle(arr) {
+    var arrLength = arr.length;
+    var t; 
+    var i;
+
+    while (arrLength) {
+        i = Math.floor(Math.random() * arrLength--);
+        t = arr[arrLength];
+        arr[arrLength] = arr[i];
+        arr[i] = t;
+    }
+    return arr;
+}
+
+function Game() {
+    this.playersGuess = null;
+    this.pastGuesses = [];
+    this.winningNumber = generateWinningNumber();
+}
+
+Game.prototype.difference = function() {
+    return Math.abs(this.playersGuess - this.winningNumber);
+}
+
+Game.prototype.isLower = function() {
+    if (this.playersGuess < this.winningNumber) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+Game.prototype.playersGuessSubmission = function(num) {
+    if (num < 1 || num > 100 || isNaN(num)) {
+        return 'That is an invalid guess.'
+    } else {
+        this.playersGuess = num;
+        return this.checkGuess();
+    }
+}
+
+Game.prototype.checkGuess = function() {
+    
+    if (this.playersGuess === this.winningNumber) {
+        return 'You Win!'
+    } else if (this.pastGuesses.includes(this.playersGuess)) {
+        return 'You have already guessed that number.'
+    }
+    
+    this.pastGuesses.push(this.playersGuess);
+
+    if (this.pastGuesses.length === 5) {
+        return 'You Lose.'
+    } 
+
+    if (this.difference() < 10) {
+        return 'You\'re so close!';
+    } else if (this.difference() < 25) {
+        return 'You\'re getting warmer!';
+    } else if (this.difference() < 50) {
+        return 'You\'re lukewarm.';
+    } else if (this.difference() < 100) {
+        return 'You\'re ice cold!';
+    }       
+}
+
+Game.prototype.provideHint = function() {
+    var hints = [];  
+    var randomHints = [];
+    for (var i = 0; i < 3; i++) {
+        randomHints.push(this.generateRandomNumber());
+    }
+    randomHints.push(this.winningNumber);
+    shuffle(randomHints);
+    hints.push('The winning number is one of: ' + randomHints);
+
+    if (this.winningNumber % 2 === 0) {
+        hints.push('It\'s an even number')
+    } else {
+        hints.push('It\'s an odd number')
+    }
+    return hints;
+}
+
+$(document).ready(function() {
+    var newGame = new Game();    
+    var takesAGuess;
+
+    $('#submit').on('click', startPlay);
+    $('.btn-danger').on('click', reset);
+    $('.btn-info').on('click', pressedHint);
+    var hintTrys = 2;
+
+    function pressedHint() {  
+        hintTrys--;
+        var giveHints = newGame.provideHint();
+        if (hintTrys >= 0) {
+            $('.hintSection').text(giveHints[hintTrys]);
+        } else {
+            $('.hintSection').text('You have no more hints left!')
+        }    
+    }
+    
+    function reset() {
+        newGame.pastGuesses = [];
+        newGame = new Game();
+        hintTrys = 2;
+        $('#player-input').attr('placeholder', '#');      
+        $('h1').text('GUESSING GAME').css({'background-color': ''});
+        $('#message').text('Go ahead, guess a number from 1 - 100:');
+        $('.guess').text('#');
+        $('#submit').attr('disabled', false);
+        $('.btn-info').attr('disabled', false);
+        $('.hintSection').text('');
+        $('.hintSection').removeClass('hidden');
+        
+    }
+
+    function startPlay() {
+        var guess = +$('#player-input').val();
+        takesAGuess = newGame.playersGuessSubmission(guess);
+        $('#player-input').val('');
+        $('.hintSection').text('');
+        $('.hintSection').removeClass('hidden');
+        
+        if (takesAGuess === 'That is an invalid guess.' || takesAGuess === 'You have already guessed that number.') {
+            $('#message').text(takesAGuess);
+        } else {
+            $('.guess:nth-child(' + newGame.pastGuesses.length + ')').text(guess);
+            if (newGame.isLower()) {
+                $('#message').text(takesAGuess + ' Guess higher!');
+            } else {
+                $('#message').text(takesAGuess + ' Guess lower!');
+            }
+        }
+        
+        if (takesAGuess === 'You Lose.') {
+            $('h1').text('YOU LOSE').css({'background-color':'red'});
+            $('#message').text('The winning number was ' + newGame.winningNumber + '. Press RESET to play again!');
+            $('#submit').attr('disabled', true);
+            $('.btn-info').attr('disabled', true);
+        } else if (takesAGuess === 'You Win!') {
+            $('h1').text('YOU WIN!!!!').css({'background-color':'green'});
+            $('#message').text('Press RESET to play again!');
+            $('#submit').attr('disabled', true);
+            $('.btn-info').attr('disabled', true);
+        } 
+    }  
+
+});
